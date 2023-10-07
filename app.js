ba4d356abdff0d9acdccd8d50055a7949c8d65aa
horusglez <horusglez@outlook.com>
2023-10-07 00:25:02 -0600

    cambios completos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola papi chulo, estas viendo porno solo, enserio?!'));
+app.get('/', (req, res) => res.send('Hola papi chulo, estas viendo porno solo, enserio?! no te la jales, papi!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
